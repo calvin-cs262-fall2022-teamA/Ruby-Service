6097612a3acc56ae44104ffec61001b5b8f51870
MatthewWalstra <mjw64@students.calvin.edu>
2022-11-08 17:47:03 -0500

    Fixed whitespace

--- rubyService.js
+++ rubyService.js
@@ -2,17 +2,13 @@
 
 // Set up the database connection.
 const pgp = require('pg-promise')();
-
-const db = pgp(  
-{
+const db = pgp({
   host: process.env.DB_SERVER,
   port: process.env.DB_PORT,
   database: process.env.DB_USER,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
 });
-
-
 
 // Configure the server and its routes.
 
